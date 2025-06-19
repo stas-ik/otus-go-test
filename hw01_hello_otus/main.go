3a9cb2dcef60f91fa8f576080b1512b93c5c1688
stas-ik <stas-anikeyev@yandex.ru>
2025-06-19 20:58:23 +0300

    HW2 is completed (fixe module name)

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -9,19 +9,27 @@ import (
 func main() {
 	fmt.Println(reverse.String("Hello, OTUS!"))
 
-	s := { {1,2,3}, {4,5,6,7,8}, {9,10,11,12,13,14,15} }
-	Concat(s)
-
-}
-
-func Concat(slises ...[]int) []int {
-	var length int
-	for _, v := range slices {
-		length += len(v)
+	s := [][]int{
+		{1, 2, 3},
+		{4, 5, 6, 7, 8},
+		{9, 10, 11, 12, 13, 14, 15},
 	}
-	sl := make([]int, 0, length)
-	return sl
+	result := Concat(s...)
+	fmt.Println(result)
 }
 
+func Concat(slices ...[]int) []int {
+	var totalLen int
+	for _, s := range slices {
+		totalLen += len(s)
+	}
 
-//сделать через copy
+	result := make([]int, totalLen)
+	pos := 0
+	for _, s := range slices {
+		copy(result[pos:], s)
+		pos += len(s)
+	}
+
+	return result
+}
