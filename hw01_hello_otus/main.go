612554b9043579d1cf704df4794f09361c07d4e6
stas-ik <stas-anikeyev@yandex.ru>
2025-06-19 14:35:08 +0300

    HW2 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -8,4 +8,20 @@ import (
 
 func main() {
 	fmt.Println(reverse.String("Hello, OTUS!"))
+
+	s := { {1,2,3}, {4,5,6,7,8}, {9,10,11,12,13,14,15} }
+	Concat(s)
+
 }
+
+func Concat(slises ...[]int) []int {
+	var length int
+	for _, v := range slices {
+		length += len(v)
+	}
+	sl := make([]int, 0, length)
+	return sl
+}
+
+
+//сделать через copy
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,73 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+
+	if unicode.IsDigit([]rune(input)[0]) {
+		return "", ErrInvalidString
+	}
+
+	result := ""
+	runes := []rune(input)
+	escape := false
+
+	for runeKey, r := range runes {
+		if escape {
+			count := 1
+			if runeKey+1 < len(runes) && unicode.IsDigit(runes[runeKey+1]) {
+				count = int(runes[runeKey+1] - '0')
+				if runeKey+2 < len(runes) && unicode.IsDigit(runes[runeKey+2]) {
+					return "", ErrInvalidString
+				}
+				runeKey++
+			}
+			for i := 1; i <= count; i++ {
+				result += string(r)
+			}
+			escape = false
+			continue
+		}
+
+		if r == '\\' {
+			escape = true
+			continue
+		}
+
+		if unicode.IsDigit(r) {
+			continue
+		}
+
+		if runeKey+1 < len(runes) {
+			nextRune := runes[runeKey+1]
+			if unicode.IsDigit(nextRune) && !escape {
+				number := int(nextRune - '0')
+				if runeKey+2 < len(runes) && unicode.IsDigit(runes[runeKey+2]) {
+					return "", ErrInvalidString
+				}
+				for i := 1; i <= number; i++ {
+					result += string(r)
+				}
+			} else if !escape {
+				result += string(r)
+			}
+		}
+
+		if runeKey == len(runes)-1 && !escape {
+			result += string(r)
+		}
+	}
+
+	if escape {
+		return "", ErrInvalidString
+	}
+
+	return result, nil
 }
