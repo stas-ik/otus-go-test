b29ab5ccf3dcf337c2b5f9b8bff508b5a589d5a9
stas-ik <stas-anikeyev@yandex.ru>
2025-06-29 17:26:40 +0300

    HW3 ready top words from text function

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	if text == "" {
+		return []string{}
+	}
+
+	words := strings.Fields(text)
+	if len(words) == 0 {
+		return []string{}
+	}
+	wordCount := make(map[string]int)
+	for _, word := range words {
+		wordCount[word]++
+	}
+
+	type wordFreq struct {
+		word  string
+		count int
+	}
+	var freqs []wordFreq
+	for word, count := range wordCount {
+		freqs = append(freqs, wordFreq{word, count})
+	}
+
+	sort.Slice(freqs, func(i, j int) bool {
+		if freqs[i].count == freqs[j].count {
+			return freqs[i].word < freqs[j].word
+		}
+		return freqs[i].count > freqs[j].count
+	})
+
+	result := make([]string, 0, 10)
+	for i := 0; i < len(freqs) && i < 10; i++ {
+		result = append(result, freqs[i].word)
+	}
+	return result
 }
