ffc7ec13dea122caa93a2618c3da96c31881ab0c
StasPPC <stas.back@teambox.email>
2025-12-15 21:06:06 +0300

    HW11 checking

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,71 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{address: address, timeout: timeout, in: in, out: out}
+}
+
+func (c *telnetClient) Connect() error {
+	d := net.Dialer{Timeout: c.timeout}
+	conn, err := d.Dial("tcp", c.address)
+	if err != nil {
+		return err
+	}
+	c.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *telnetClient) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+	return nil
+}
+
+func (c *telnetClient) Send() error {
+	if c.conn == nil {
+		return io.ErrClosedPipe
+	}
+	r := bufio.NewReader(c.in)
+	b, err := r.ReadBytes('\n')
+	if len(b) > 0 {
+		if _, werr := c.conn.Write(b); werr != nil {
+			return werr
+		}
+	}
+	if err != nil && err != io.EOF {
+		return err
+	}
+	if err == io.EOF && len(b) == 0 {
+		return io.EOF
+	}
+	return nil
+}
+
+func (c *telnetClient) Receive() error {
+	if c.conn == nil {
+		return io.ErrClosedPipe
+	}
+	r := bufio.NewReader(c.conn)
+	b, err := r.ReadBytes('\n')
+	if len(b) > 0 {
+		if _, werr := c.out.Write(b); werr != nil {
+			return werr
+		}
+	}
+	if err != nil && err != io.EOF {
+		return err
+	}
+	if err == io.EOF && len(b) == 0 {
+		return io.EOF
+	}
+	return nil
+}
